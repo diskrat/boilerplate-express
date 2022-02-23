74b73e11b02798958edd3a4f2a48ff99aa6cdf33
Joao <joao.silva.123@ufrn.edu.br>
2022-02-23 20:43:04 -0300

    send file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/',hello)
 
-function hello(req,res) {
-    res.send("Hello Express")
+function views(req,res) {
+    res.sendFile(__dirname+ '/views/index.html')
 }
+app.get('/',views)
+
+
+
+
 
 
 
