4cf4e11ccda4f7b1856459937d0c4f0e43629736
Joao <joao.silva.123@ufrn.edu.br>
2022-02-23 21:00:46 -0300

    static

--- myApp.js
+++ myApp.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 
 
-style = express.static(__dirname + '/public')
-
 function views(req,res) {
     res.sendFile(__dirname+ '/views/index.html')
 }
-app.use('/',style)
+app.use('/',express.static(__dirname + '/public'))
 app.get('/',views)
 
 
