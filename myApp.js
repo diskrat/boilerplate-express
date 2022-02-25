5a06835633cbbdc1fe7d6f73bf3446fb84a328ad
Joao <joao.silva.123@ufrn.edu.br>
2022-02-25 10:37:37 -0300

    get json implement

--- myApp.js
+++ myApp.js
@@ -5,10 +5,13 @@ var app = express();
 function views(req,res) {
     res.sendFile(__dirname+ '/views/index.html')
 }
+function get_json(req,res) {
+    res.json({"message": "Hello json"})
+}
+
 app.use('/public',express.static(__dirname + '/public'))
 app.get('/',views)
-
-
+app.get('/json',get_json);
 
 
 
